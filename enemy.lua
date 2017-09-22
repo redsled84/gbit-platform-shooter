881b404dd76df00a66e95a744609d6a0eccdda37
redsled84 <hmnotsocool@gmail.com>
2017-09-22 09:41:31 -0700

    behavior trees WIP

--- enemy.lua
+++ enemy.lua
@@ -9,18 +9,78 @@ do
   local _class_0
   local _parent_0 = Entity
   local _base_0 = {
-    getNextGoal = function(self) end,
+    getNextGoal = function(self)
+      return self.direction > 0 and self.goals[self.goalCounter + 1] or self.goals[self.goalCounter - 1]
+    end,
     removed = false,
     damage = function(self, amount)
       self.health = self.health - amount < 0 and 0 or self.health - amount
     end,
-    movementSequence = BT:new({
-      object = self,
-      tree = BT.Sequence:new({
-        nodes = { }
+    movementSequence = function(self)
+      return BT:new({
+        object = self,
+        tree = BT.Sequence:new({
+          nodes = {
+            BT.Task:new({
+              run = function(task, object)
+                object.currentGoal = object:getNextGoal()
+                return task:success()
+              end
+            }),
+            BT.Sequence:new({
+              nodes = {
+                BT.Task:new({
+                  run = function(task, object)
+                    object.dx = object.getNextGoal().x - object.x
+                    object.dy = object.y - object.getNextGoal().y
+                    return task:success()
+                  end
+                }),
+                BT.Priority:new({
+                  nodes = {
+                    BT.Sequence:new({
+                      nodes = {
+                        BT.Task:new({
+                          run = function(task, object)
+                            if object.dy > 0 then
+                              return task:success()
+                            else
+                              return task:fail()
+                            end
+                          end
+                        }),
+                        BT.Task:new({
+                          run = function(task, object)
+                            object.vy = object.jumpVelocity
+                            return task:success()
+                          end
+                        })
+                      }
+                    }),
+                    BT.Task:new({
+                      run = function(task, object)
+                        if object.dx > 0 then
+                          object.vx = object.walkSpeed
+                          return task:success()
+                        elseif object.dx < 0 then
+                          object.vx = -object.walkSpeed
+                          return task:success()
+                        else
+                          object.vx = 0
+                          return task:fail()
+                        end
+                      end
+                    })
+                  }
+                })
+              }
+            })
+          }
+        })
       })
-    }),
+    end,
     update = function(self, dt)
+      self:movementSequence():run()
       self:updateGravity(dt, gravity, terminalVelocity)
       if not self.removed then
         local goalX, goalY, cols, len
@@ -50,12 +110,19 @@ do
   _base_0.__index = _base_0
   setmetatable(_base_0, _parent_0.__base)
   _class_0 = setmetatable({
-    __init = function(self, goals, sprite, health)
-      self.goals, self.sprite, self.health = goals, sprite, health
-      self.currentGoal = self.goals[1]
-      self.goalCounter = 1
+    __init = function(self, goals, sprite, health, goalCounter)
+      if goalCounter == nil then
+        goalCounter = 1
+      end
+      self.goals, self.sprite, self.health, self.goalCounter = goals, sprite, health, goalCounter
+      self.currentGoal = self.goals[self.goalCounter]
       self.direction = 1
       self.vx = 100
+      self.vy = 0
+      self.jumpVelocity = -400
+      self.dx = 0
+      self.dy = 0
+      self.walkSpeed = 100
       return _class_0.__parent.__init(self, self.currentGoal.x, self.currentGoal.y, nil, nil, self.sprite:getWidth(), self.sprite:getHeight() * 2, {
         255,
         255,
--- enemy.moon
+++ enemy.moon
@@ -7,26 +7,82 @@ gravity = 1000
 terminalVelocity = 750
 
 class Enemy extends Entity
-	new: (@goals, @sprite, @health) =>
-		@currentGoal = @goals[1]
-		@goalCounter = 1
+	new: (@goals, @sprite, @health, @goalCounter=1) =>
+		@currentGoal = @goals[@goalCounter]
 		-- 1 FOR GOING TO RIGHT, -1 FOR GOING TO LEFT
 		@direction = 1
 		@vx = 100
+		@vy = 0
+		@jumpVelocity = -400
+		@dx = 0
+		@dy = 0
+		@walkSpeed = 100
 		super @currentGoal.x, @currentGoal.y, nil, nil, @sprite\getWidth!, @sprite\getHeight! * 2, {255, 255, 255}, @sprite
 	getNextGoal: =>
+		return @direction > 0 and @goals[@goalCounter+1] or @goals[@goalCounter-1]
 	removed: false
 	damage: (amount) =>
 		@health = @health - amount < 0 and 0 or @health - amount
-	movementSequence: BT\new {
-		object: self,
-		tree: BT.Sequence\new {
-			nodes: {
-
+	movementSequence: =>
+		BT\new {
+			object: self
+			tree: BT.Sequence\new {
+				nodes: {
+					BT.Task\new {
+						run: (task, object) ->
+							object.currentGoal = object.getNextGoal!
+							task\success!
+					}
+					BT.Sequence\new {
+						nodes: {
+							BT.Task\new {
+								run: (task, object) ->
+									local nextGoal
+									nextGoal = object.getNextGoal
+									object.dx = nextGoal.x - object.x
+									object.dy = object.y - nextGoal.y
+									task\success!
+							}
+							BT.Priority\new {
+								nodes: {
+									BT.Sequence\new {
+										nodes: {
+											BT.Task\new {
+												run: (task, object) ->
+													if object.dy > 0
+														task\success!
+													else
+														task\fail!
+											}
+											BT.Task\new {
+												run: (task, object) ->
+													object.vy = object.jumpVelocity
+													task\success!
+											}
+										}
+									}
+									BT.Task\new {
+										run: (task, object) ->
+											if object.dx > 0
+												object.vx = object.walkSpeed
+												task\success!
+											elseif object.dx < 0
+												object.vx = -object.walkSpeed
+												task\success!
+											else
+												object.vx = 0
+												task\fail!
+									}
+								}
+							}
+						}
+					}
+				}
 			}
 		}
-	}
+
 	update: (dt) =>
+		@movementSequence!\run!
 		@updateGravity dt, gravity, terminalVelocity
 
 		if not @removed
